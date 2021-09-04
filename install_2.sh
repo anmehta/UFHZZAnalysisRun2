16f275bf6f794e30df65b2088b938eb3b5ae0b32
jialin guo <guoj@cern.ch>
2021-09-04 14:38:16 +0200

    add JetToolBox

--- install_2.sh
+++ install_2.sh
@@ -14,6 +14,7 @@ git checkout -b from-v223 v2.2.3
 cd ../
 git clone -b RunII_v2 https://github.com/VBF-HZZ/UFHZZAnalysisRun2-Accessary.git
 mv UFHZZAnalysisRun2-Accessary/* ./
+rm -rf UFHZZAnalysisRun2-Accessary
 mv UFHZZAnalysisRun2-Accessary/RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_Autumn18_ID_ISO_cff.py ./RecoEgamma/ElectronIdentification/python/Identification/
 mv UFHZZAnalysisRun2-Accessary/RecoEgamma/ElectronIdentification/python/Identification/mvaElectronID_Summer16_ID_ISO_cff.py  ./RecoEgamma/ElectronIdentification/python/Identification/
 mv UFHZZAnalysisRun2-Accessary/RecoEgamma/ElectronIdentification/python/ElectronMVAValueMapProducer_cfi.py  RecoEgamma/ElectronIdentification/python/
@@ -21,4 +22,5 @@ mv UFHZZAnalysisRun2-Accessary/RecoEgamma/ElectronIdentification/python/Electron
 mv UFHZZAnalysisRun2-Accessary/RecoEgamma/ElectronIdentification/data/MVAWeightFiles RecoEgamma/ElectronIdentification/data/
 mv UFHZZAnalysisRun2-Accessary ../
 git clone -b tmp_Ferrico https://github.com/ferrico/KinZfitter.git
+git clone git@github.com:cms-jet/JetToolbox.git JMEAnalysis/JetToolbox -b jetToolbox_102X_v3
 scram b -j 8
