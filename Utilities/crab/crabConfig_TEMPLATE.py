7cabc3ff40793b436cc9c635e38df8c82aff9a67
qianying guo <qyguo@buaa.edu.cn>
2021-12-28 15:37:05 +0100

    update

--- Utilities/crab/crabConfig_TEMPLATE.py
+++ Utilities/crab/crabConfig_TEMPLATE.py
@@ -1,6 +1,7 @@
 import cjson
 import json
 from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config, getUsernameFromCRIC
 
 config = Configuration()
 
@@ -15,7 +16,9 @@ import os
 config.section_('JobType')
 config.JobType.scriptExe = 'submitFileCrab.sh'
 #config.JobType.inputFiles = [os.environ.get('CMSSW_BASE')+'/src/ZZMatrixElement/MEKD',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/KinZfitter/ParamZ1',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/HelperFunction/hists',os.environ.get('CMSSW_BASE')+'/src/ZZMatrixElement/MELA']
-config.JobType.inputFiles = [os.environ.get('CMSSW_BASE')+'/src/ZZMatrixElement/MEKD',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/KinZfitter/ParamZ1',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/HelperFunction/hists']
+#config.JobType.inputFiles = [os.environ.get('CMSSW_BASE')+'/src/ZZMatrixElement/MEKD',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/KinZfitter/ParamZ1',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/HelperFunction/hists']
+#config.JobType.inputFiles = [os.environ.get('CMSSW_BASE')+'/src/JHUGenMELA/MELA',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/KinZfitter/ParamZ1',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/HelperFunction/hists']
+config.JobType.inputFiles = [os.environ.get('CMSSW_BASE')+'/src/KinZfitter/KinZfitter/ParamZ1',os.environ.get('CMSSW_BASE')+'/src/KinZfitter/HelperFunction/hists']
 config.JobType.psetName = 'CFGFILE'
 config.JobType.pluginName = 'Analysis'
 config.JobType.disableAutomaticOutputCollection = True
@@ -28,16 +31,19 @@ config.Data.inputDataset = 'DATASETNAME'
 if ('Run2018' in 'DATASETNAME'):
   #config.Data.lumiMask = 'Cert_314472-323523_13TeV_PromptReco_Collisions18_JSON.txt'##Moriond18
   #config.Data.lumiMask = 'Run2016_ReRecoJSON.txt'
-  config.Data.lumiMask = 'Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'###Legacy 18
+  #config.Data.lumiMask = 'Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'###Legacy 18
+  config.Data.lumiMask = 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
   config.Data.splitting = 'EventAwareLumiBased'
   config.Data.unitsPerJob = 100000
 elif ('Run2017' in 'DATASETNAME'):
-  config.Data.lumiMask = 'Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'###Legacy 17
+  #config.Data.lumiMask = 'Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'###Legacy 17
+  config.Data.lumiMask = 'Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'
   config.Data.splitting = 'EventAwareLumiBased'
   config.Data.unitsPerJob = 100000
 elif ('Run2016' in 'DATASETNAME'):
   #config.Data.lumiMask = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'###Legacy 16
-  config.Data.lumiMask = 'Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt'###Legacy 16 changed
+  #config.Data.lumiMask = 'Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt'###Legacy 16 changed
+  config.Data.lumiMask = 'Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'
   config.Data.splitting = 'EventAwareLumiBased'
   config.Data.unitsPerJob = 100000
 else:
@@ -46,16 +52,18 @@ else:
   elif (('GluGlu' in 'DATASETNAME') and ('MCFM701' in 'DATASETNAME') and ('tau' in 'DATASETNAME')): config.Data.unitsPerJob = 1
   elif (('GluGlu' in 'DATASETNAME') and (not 'MCFM701' in 'DATASETNAME')): config.Data.unitsPerJob = 1
   elif ('HToZZ' in 'DATASETNAME'): config.Data.unitsPerJob = 1
-  elif ('ZZ' in 'DATASETNAME'): config.Data.unitsPerJob = 2
-  elif ('TT' in 'DATASETNAME'): config.Data.unitsPerJob = 2
-  else: config.Data.unitsPerJob = 2
+  elif ('ZZ' in 'DATASETNAME'): config.Data.unitsPerJob = 4
+  elif ('TT' in 'DATASETNAME'): config.Data.unitsPerJob = 5
+  else: config.Data.unitsPerJob = 5
 
 config.Data.publication = False
-config.Data.outLFNDirBase = '/store/user/ferrico/VxBS_withRochester/UFHZZAnalysisRun2/JOBTAG/'
+#config.Data.outLFNDirBase = '/store/user/%s/2018data/UFHZZAnalysisRun2/JOBTAG/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/qguo/newNTuple_UL/UL2018/UFHZZAnalysisRun2/JOBTAG/'
 config.Data.ignoreLocality = True
 config.Data.allowNonValidInputDataset = True
 
 config.section_('User')
 config.section_('Site')
+#config.Site.storageSite = 'T2_CN_Beijing'
 config.Site.storageSite = 'T2_US_Florida'
 config.Site.whitelist = ['T2_US_*']
